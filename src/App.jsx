745f1644e7902f4e80ef97001d0f38f3a994dedc
ALEX ADRIAN <alexadrianchy@LAPTOP-K95VR1IJ.localdomain>
2024-06-20 11:30:19 -0500

    nuevo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from './componente/Home'
+/* import './App.css' */
+import About from './componente/About'
+import Dashbard from './componente/Dashboard'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <About/>
+      <Dashbard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componente/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashbard() {
+
+  return (
+    <>
+      <div>
+        <h1>
+            Dashboard
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default Dashbard
